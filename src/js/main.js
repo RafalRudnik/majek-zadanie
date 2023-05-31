4ac31f5f14473422c5e87f28295f50e8267ba89b
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-05-31 11:41:33 +0200

    footer section

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const burgerBtn = document.querySelector('.nav__button span');
 const navList = document.querySelector('.nav__list-mobile');
 const allLinks = document.querySelectorAll('.nav__link-mobile');
 const currentYear = document.querySelector('.current-year');
+const links = document.querySelectorAll('.nav__link');
 
 const handleBurger = () => {
 	burgerBtn.classList.toggle('spanActive');
@@ -17,5 +18,11 @@ const handleCurrentYear = () => {
 	currentYear.textContent = year;
 };
 
+const handleScroller = (e) => {
+	links.forEach((link) => link.classList.remove('activeLink'));
+	e.target.classList.add('activeLink');
+};
+
 handleCurrentYear();
 burgerBtn.addEventListener('click', handleBurger);
+links.forEach((item) => item.addEventListener('click', handleScroller));
