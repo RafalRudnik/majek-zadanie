0b4e2339f7c0f5d4d30f9ed5337261a194cc6a4d
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-06-04 11:32:29 +0200

    bug fixes

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ const navList = document.querySelector('.nav__list-mobile');
 const allLinks = document.querySelectorAll('.nav__link-mobile');
 const currentYear = document.querySelector('.current-year');
 const links = document.querySelectorAll('.nav__link');
+const input = document.getElementById('name');
+const mailInput = document.getElementById('email');
+const msgInput = document.getElementById('msg');
+const error = document.querySelector('.msg-status');
+const sendBtn = document.querySelector('.sendBtn');
+const allInputs = document.querySelectorAll('input');
 
 // SCROLLER
 
@@ -79,7 +85,34 @@ const listHandle = () => {
 	});
 };
 
+const inputCheck = () => {
+	if (input.value === '') {
+		input.placeholder = 'Musisz podać imię';
+		input.classList.add('input-error');
+	} else {
+		input.placeholder = '';
+		input.classList.remove('input-error');
+	}
+};
+
+const emailCheck = (mailInput) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/i;
+	if (re.test(mailInput.value)) {
+		mailInput.placeholder = '';
+		mailInput.classList.remove('input-error');
+	} else {
+		mailInput.placeholder = 'Musisz podać poprawny email';
+		mailInput.classList.add('input-error');
+	}
+};
+
 handleCurrentYear();
 listHandle();
+sendBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	inputCheck();
+	emailCheck(mailInput);
+});
 burgerBtn.addEventListener('click', handleBurger);
 window.addEventListener('scroll', handleObserver);
